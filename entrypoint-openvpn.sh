22290db3bb98ea93719368969772af0319c0340b
Lauri Võsandi <lauri@pinecrypt.com>
2021-06-05 04:30:38 +0000

    Remove CI/CD related files

--- entrypoint-openvpn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-sleep 10
-$@
-AUTHORITY=$3
-test -f /etc/certidude/authority/$AUTHORITY/host_cert.pem
-openvpn --config /etc/openvpn/$AUTHORITY.conf
--- entrypoint-strongswan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-sleep 10
-$@
-AUTHORITY=$3
-test -f /etc/certidude/authority/$AUTHORITY/host_cert.pem
-/usr/sbin/ipsec stop
-/usr/sbin/ipsec start --nofork
